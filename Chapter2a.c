a4b1ec0e22f187de522ad20df7aea373bc12b35e
Andrew Smith <AndrewSmithWasTaken@users.noreply.github.com>
2015-11-22 10:49:32 +0200

    Create Chapter2a.c

---
+++ Chapter2a.c
@@ -0,0 +1,13 @@
+Write a functioning program in C that, upon execution, prints the following on the screen: "Hello everybody!",
+followed by a newline character \n after the exclamation point. After printing, the program closes itself.
+
+Example output:
+Hello everybody!
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello everybody!\n");
+	return 0;
+}
