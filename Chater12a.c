e63e55294f8cf11197f24cca93cab9a2b6fe29e5
Andrew Smith <AndrewSmithWasTaken@users.noreply.github.com>
2015-11-22 11:10:52 +0200

    Update Chater12a.c

--- Chater12a.c
+++ Chater12a.c
@@ -1,6 +1,6 @@
 /*The following is a simple C program that prompts the user for an integer and stores it in the variable x.
-The program prints the number, calls the subroutine "change_number" passing the number as an argument,
-and finally prints the number again:
+The program prints the number, calls the subroutine "change_number" passing the number as an
+argument, and finally prints the number again:
 
 #include<stdio.h>
 
@@ -16,9 +16,9 @@ int main()
   printf("In the main program: x = %d\n", x);
 }
 
- Your task is to write the subroutine "change_number" that takes an integer, increases it by three
- and prints the changed number as shown in the example printout. Pay attention to the value of the
- variable "x" in different parts of the program. In the following exercise, your subroutine will be supplemented with pointers.
+ Your task is to write the subroutine "change_number" that takes an integer, increases it by three and
+ prints the changed number as shown in the example printout. Pay attention to the value of the variable
+ "x" in different parts of the program. In the following exercise, your subroutine will be supplemented with pointers.
 
 Example output:
 Enter the number x: 111
@@ -26,9 +26,9 @@ In the main program: x = 111
 In the subroutine: x = 114
 In the main program: x = 111*/
 
-#include <stdio.h>
+#include<stdio.h>
 
-void change_number(int *x);
+void change_number(int x);
 
 int main()
 {
@@ -36,12 +36,13 @@ int main()
   printf("Enter the number x: ");
   scanf("%d", &x);
   printf("In the main program: x = %d\n", x);
-  change_number(&x);
+  change_number(x);
   printf("In the main program: x = %d\n", x);
+  return 0;
 }
 
-void change_number(int *x)
+void change_number(int x)
 {
-	*x +=3;
-	printf("In the subroutine: x = %d\n", *x);
+	x +=3;
+	printf("In the subroutine: x = %d\n", x);
 }
